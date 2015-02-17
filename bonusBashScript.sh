d01320416dee94a260b9f95dc7495412bbf4fa4b
iancballard <iancballard@gmail.com>
2015-02-16 21:01:39 -0800

    updated code to do analysis for blocking version of FD

--- bonusBashScript.sh
+++ bonusBashScript.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env sh
 pushd /Applications/aws-mturk-clt-1.3.1/bin
-./grantBonus.sh -workerid A31Z5TPD8QKE26 -amount 2.85 -assignment 338JKRMM26ZNJAV3P7SY02LMTDSAH3 -reason "Your bonus for performing my hit. Congratulations on your performance!" 
-./grantBonus.sh -workerid A175PJR0W3LO8I -amount 2.75 -assignment 3OS4RQUCR9F8OO9P0V4USSTFVG2FB7 -reason "Your bonus for performing my hit. Congratulations on your performance!" 
-./grantBonus.sh -workerid A3R25SV8HUGML2 -amount 2.45 -assignment 3WOKGM4L71G1AEJ81IQVV56AWDUO0E -reason "Your bonus for performing my hit. Congratulations on your performance!" 
-./grantBonus.sh -workerid A3NMQ3019X6YE0 -amount 2.65 -assignment 3QECW5O0KH1GIDAHU5I66A8OCTMT5Q -reason "Your bonus for performing my hit. Congratulations on your performance!" 
+./grantBonus.sh -workerid AKX5RHHO8BIUX -amount 3.2 -assignment 3IKZ72A5B4GAPXYM3L93JNT9FANNFO -reason "Your bonus for performing my hit. Congratulations on your performance!" 
+./grantBonus.sh -workerid A2OG7XGWOX5ZEF -amount 1.9 -assignment 3A4TN5196KIBCPS19LOP6XBAWLGCHN -reason "Your bonus for performing my hit. Congratulations on your performance!" 
+./grantBonus.sh -workerid A100VV1V2XDQI7 -amount 0.95 -assignment 33LK57MYLT5DARHRCTKWL9JXEERZS2 -reason "Your bonus for performing my hit. Congratulations on your performance!" 
+./grantBonus.sh -workerid AI1MF6MS74XFY -amount 0.9 -assignment 3ATTHHXXWAOZBIEG6Z2XKMXZXGNIXS -reason "Your bonus for performing my hit. Congratulations on your performance!" 
+./grantBonus.sh -workerid A3FFAWIZENG1FG -amount 0.1 -assignment 3LYA37P8IQNJ4A1316VR85LK6HJBKI -reason "Your bonus for performing my hit. Congratulations on your performance!" 
+./grantBonus.sh -workerid A2FUPODRRCESB -amount 1.2 -assignment 3TAYZSBPLL8N4GEFNMBIYY9QT5V2S0 -reason "Your bonus for performing my hit. Congratulations on your performance!" 
+./grantBonus.sh -workerid A2MKXI4KCRRI7Y -amount 1.35 -assignment 3STRJBFXOWRJ0WVK1SMC8ZR6CORKTP -reason "Your bonus for performing my hit. Congratulations on your performance!" 
+./grantBonus.sh -workerid A1ZRK6K5JUPJAV -amount 1.4 -assignment 3WQ3B2KGE8GHY9FHTWP93QPLXBP1BB -reason "Your bonus for performing my hit. Congratulations on your performance!" 
+./grantBonus.sh -workerid AVI7K876BV3QL -amount 0.85 -assignment 3EKVH9QMEY4H2RVXQWGT3ZNQS2A2DV -reason "Your bonus for performing my hit. Congratulations on your performance!" 
+./grantBonus.sh -workerid A256FHXGSY0E5D -amount 5.4 -assignment 3VP0C6EFSGW8OMKBQRRZC2RWHLAM6K -reason "Your bonus for performing my hit. Congratulations on your performance!" 
+./grantBonus.sh -workerid A22W435NF50DJ5 -amount 3.2 -assignment 3AZHRG4CU4KCOPQMIK63728Z4R503F -reason "Your bonus for performing my hit. Congratulations on your performance!" 
+./grantBonus.sh -workerid ABB9DYBUWGNNX -amount 0.3 -assignment 3YJ6NA41JBGQXKWV2QDG2AQW8W6PJQ -reason "Your bonus for performing my hit. Congratulations on your performance!" 
 popd
